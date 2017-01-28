7f0a06ac25ee6db9fb13d0aac7ffa5c09d7ab84d
Blake Embrey <hello@blakeembrey.com>
2017-01-28 13:53:46 -0800

    Add arity to proxy function name

--- arity.js
+++ arity.js
@@ -11,13 +11,17 @@ module.exports = function (arity, fn) {
   if (!FUNCTIONS[arity]) {
     var params = [];
 
+    if (typeof arity !== 'number') {
+      throw new TypeError('Expected arity to be a number, got ' + arity);
+    }
+
     for (var i = 0; i < arity; i++) {
       params.push('_' + i);
     }
 
     FUNCTIONS[arity] = new Function(
       'fn',
-      'return function (' + params.join(', ') + ') { return fn.apply(this, arguments); }'
+      'return function arity' + arity + ' (' + params.join(', ') + ') { return fn.apply(this, arguments); }'
     );
   }
 
